04f772cf789cbc1037176f1e5ac0d48a2f6089c4
michaelmarigliano <michael@sharkcell.dk>
2020-09-30 14:52:00 +0200

    linkin lonks work and email clipboarding works. just missing bios and stuff

--- captis/src/components/WhoWeAre.vue
+++ captis/src/components/WhoWeAre.vue
@@ -14,15 +14,13 @@
                         <v-card
                                 class="people-cards mx-auto"
                                 elevation="0"
-                                hover="false"
-                                outlined="false"
                         >
 
                             <v-card-title class="people-names">{{person.name}}</v-card-title>
-                            <v-card-subtitle class="people-descriptions">{{person.description}}</v-card-subtitle>
+                            <v-card-subtitle class="people-descriptions text-center">{{person.description}}</v-card-subtitle>
                             <span>
-                            <v-icon class = "links" small color="#61BFF0" v-on:click="linkedInClick()">mdi-linkedin</v-icon>
-                            <v-icon class = "links" small color="#61BFF0" v-on:click="emailClick()">mdi-email</v-icon>
+                            <v-icon class = "links" small color="#61BFF0"  v-on:click="linkedInClick(person.linkdIn)">mdi-linkedin</v-icon>
+                            <v-icon class = "links" small color="#61BFF0" v-on:click="emailClick(person.email, person.name)">mdi-email</v-icon>
                             <v-icon class = "links" small color="#61BFF0" v-on:click="moreInfoClick()">mdi-information-outline</v-icon>
                                 </span>
                         </v-card>
@@ -47,7 +45,7 @@
                         description: "Wind Energy",
                         picture: "a.jpg",
                         linkdIn: "https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fartur-s-zebrowski%2F%3Ffbclid%3DIwAR2tEib6aC67nSBaCnVEH1rnQohN8L86X6Rd-aqyixczkBhTkFBaZK8VZoY&h=AT3o0ZB4CdsxnuOIPwAcITDyek8eoZ-PEeAlUkPe6HNhAgxswpmMkZPG-0t_sO06WR9iy7vxPwan9-HeFts6pureelOIEZCAGlcd_5PPG1avGQoUNPPVGH2nhU3ZTrYbSUYmUcPH",
-                        email:"",
+                        email:"s202371@student.dtu.dk",
                     },
                     {
                         id: 1,
@@ -55,7 +53,7 @@
                         description: "Product Development",
                         picture: "b.jpg",
                         linkdIn: "https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fburkay-duymaz-152b26152%2F%3Ffbclid%3DIwAR1n1ArAzq1alCD4kaMsBtwgGpEl9JxSkWNvrrfKtBKoySHpvgKGLfbQcxc&h=AT3o0ZB4CdsxnuOIPwAcITDyek8eoZ-PEeAlUkPe6HNhAgxswpmMkZPG-0t_sO06WR9iy7vxPwan9-HeFts6pureelOIEZCAGlcd_5PPG1avGQoUNPPVGH2nhU3ZTrYbSUYmUcPH",
-                        email:"",
+                        email:"s165036@student.dtu.dk",
                     },
                     {
                         id: 2,
@@ -63,7 +61,7 @@
                         description: "Chemical Engineering",
                         picture: "c.jpg",
                         linkdIn: "https://l.facebook.com/l.php?u=http%3A%2F%2Flinkedin.com%2Fin%2Fcyril-estok%3Ffbclid%3DIwAR2rAeu0-CaJclt0S4xYipY6i3hxApkXgS0hIh0D7mNDphNyVqCOBMZ_aE8&h=AT3o0ZB4CdsxnuOIPwAcITDyek8eoZ-PEeAlUkPe6HNhAgxswpmMkZPG-0t_sO06WR9iy7vxPwan9-HeFts6pureelOIEZCAGlcd_5PPG1avGQoUNPPVGH2nhU3ZTrYbSUYmUcPH",
-                        email:"",
+                        email:"s164562@student.dtu.dk",
                     },
                     {
                         id: 3,
@@ -71,7 +69,7 @@
                         description: "Software Engineering",
                         picture: "m.jpg",
                         linkdIn: "https://www.linkedin.com/in/michael-marigliano-b22a2b1ab/",
-                        email:"",
+                        email:"s202787@student.dtu.dk",
                     },
                     {
                         id: 4,
@@ -79,15 +77,15 @@
                         description: "Physics and Nanotechnology​",
                         picture: "n.jpg",
                         linkdIn: "https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fnathan-moret-456464184%3Ffbclid%3DIwAR3hS95T7didCfOMaiTpZG03-SWcUoLhpjSuxURZrQV2TeAUlitWhHT8hhQ&h=AT3o0ZB4CdsxnuOIPwAcITDyek8eoZ-PEeAlUkPe6HNhAgxswpmMkZPG-0t_sO06WR9iy7vxPwan9-HeFts6pureelOIEZCAGlcd_5PPG1avGQoUNPPVGH2nhU3ZTrYbSUYmUcPH",
-                        email:"",
+                        email:"s202935@student.dtu.dk",
                     },
                     {
                         id: 6,
                         name: "Zainab Hassoun",
                         description: "Autonomous Systems Engineering",
                         picture: "z.jpg",
-                        linkdIn: "",
-                        email:"",
+                        linkdIn: "http://linkedin.com",
+                        email:"s165036@student.dtu.dk",
                     },
                     {
                         id: 5,
@@ -95,17 +93,25 @@
                         description: "Materials and Manufacturing​",
                         picture: "o.jpg",
                         linkdIn: "https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.linkedin.com%2Fin%2Folekleinschmidt%2F%3Ffbclid%3DIwAR2IY3K0yw4tEHa_6V7VvPEvqRjSttI7q0U03rE5wE_ucKuPvL6mrlhh9Hg&h=AT3o0ZB4CdsxnuOIPwAcITDyek8eoZ-PEeAlUkPe6HNhAgxswpmMkZPG-0t_sO06WR9iy7vxPwan9-HeFts6pureelOIEZCAGlcd_5PPG1avGQoUNPPVGH2nhU3ZTrYbSUYmUcPH",
-                        email:"",
+                        email:"s192852@student.dtu.dk",
                     },
                 ]
             }
         },
         methods: {
-            linkedInClick(){
-                console.log("linkedIn icon clicked");
+            linkedInClick(linkdInLink){
+                console.log("link click works: " + linkdInLink);
+                window.open(linkdInLink);
             },
-            emailClick(){
-                console.log("email icon clicked");
+            emailClick(email, name){
+                console.log("email link works: " + email);
+                this.$copyText(email).then(function (e) {
+                    alert('Email for ' + name + ' copied to your clipboard!' );
+                    console.log(e)
+                }, function (e) {
+                    alert('Could not retrieve email');
+                    console.log(e)
+                });
             },
             moreInfoClick(){
                 console.log("more info icon clicked");
@@ -144,7 +150,7 @@
     align-content: center;
     justify-items: center;
     /* top | right | bottom | left */
-    margin: 20px 10% 0 10%;
+    margin: 5px 10% 0 10%;
     /*min-width: 200px;*/
     /*max-width: 200px;*/
     /*min-height: 100px;*/
@@ -171,6 +177,7 @@
     font-variant: all-petite-caps;
     font-size: 18px;
     alignment-baseline: top;
+    justify-content: center;
     color: #61BFF0;
     top: 0;
     bottom: 0;
@@ -180,9 +187,8 @@
 }
 .people-descriptions{
     font-family: Optima, serif;
-    font-size: 10px;
+    font-size: 12px;
     color: gray;
-    marigin: 0;
 }
 .people-descriptions:hover{
     cursor: default;
