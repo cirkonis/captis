438ada46649225383bb07d9a592026517a85568a
michaelmarigliano <michael@sharkcell.dk>
2020-10-20 17:12:39 +0200

    workin on the d3

--- captis/src/components/Carbon.vue
+++ captis/src/components/Carbon.vue
@@ -1,8 +1,6 @@
 <template>
     <v-container class="svg-container">
         <svg class="carbon-svg">
-            <g :id="nodes"></g>
-            <g :id="links"></g>
         </svg>
     </v-container>
 </template>
@@ -11,68 +9,28 @@
     import * as d3 from "d3";
     export default {
         name: "Carbon",
+
         data: function () {
-            return {
-                nodes: [
-                    {
-                    id: 1,
-                    type: 'c1'
-                },
-                    {
-                        id: 2,
-                        type: 'c2'
-                },
-                    {
-                        id: 3,
-                        type: 'o'
-                    }
-                ],
-                links: [
-                    {
-                        id: 10,
-                        source: 1,
-                        target: 2,
-                    },
-                    {
-                        id: 11,
-                        source: 3,
-                        target: 1,
-                    },
-                ],
-            }
+            return {}
         },
         mounted() {
-           // this.makeGraph('.carbon-svg');
-            let svg = d3.select('svg');
-            // let width = svg.attr('width');
-            // let height = svg.attr('height');
+            let svg = d3.select('.carbon-svg');
             let g = svg.append('g');
-            let nodes = [
-                { name: '湖南邵阳' },
-                { name: '山东泰安' },
-                { name: '广东阳江' },
-                { name: '山西太原' },
-                { name: '亮' },
-                { name: '丽' },
-                { name: '雪' },
-                { name: '小明' },
-                { name: '组长' }
-            ];
-            let edges = [
-                { source: 0, target: 4, relation: '籍贯', value: 1.3 },
-                { source: 4, target: 5, relation: '舍友', value: 1 },
-                { source: 4, target: 6, relation: '舍友', value: 1 },
-                { source: 4, target: 7, relation: '舍友', value: 1 },
-                { source: 1, target: 6, relation: '籍贯', value: 2 },
-                { source: 2, target: 5, relation: '籍贯', value: 0.9 },
-                { source: 3, target: 7, relation: '籍贯', value: 1 },
-                { source: 5, target: 6, relation: '同学', value: 1.6 },
-                { source: 6, target: 7, relation: '朋友', value: 0.7 },
-                { source: 6, target: 8, relation: '职责', value: 2 }
-            ];
-            let colorScale = d3.scaleOrdinal()
-                .domain(d3.range(nodes.length))
-                .range(d3.schemeCategory10);
+            let bonds = [];
+            let numNodes = Math.pow(3, 5);
+            let nodes = [];
+            if(numNodes % 3 === 0) {
+                for (let i = 0; i < numNodes; i += 3) {
+                    nodes.push({id: i, type: 'c1'});
+                    nodes.push({id: i + 1, type: 'c2'});
+                    nodes.push({id: i + 2, type: 'o'});
+                    bonds.push({source: i, target: i+2});
+                    bonds.push({source: i + 1, target: i+2});
+                }
+            }
+            let edges = bonds;
+            console.log(nodes);
+            console.log(edges);
             let forceSimulation = d3.forceSimulation()
                 .force('link', d3.forceLink())
                 .force('charge', d3.forceManyBody())
@@ -81,29 +39,17 @@
                 .on('tick', ticked);
             forceSimulation.force('link')
                 .links(edges)
-                .distance(function (d) { // 每一边的长度
-                    return d.value * 100
-                });
+                .distance(.5);
             forceSimulation.force('center')
-                .x(150)
-                .y(150);
+                .x(window.innerWidth / 2)
+                .y(125);
             let links = g.append('g')
                 .selectAll('line')
                 .data(edges)
                 .enter()
                 .append('line')
-                .attr('stroke', function (d, i) {
-                    return colorScale(i)
-                })
-                .attr('stroke-width', 1);
-            let linksText = g.append('g')
-                .selectAll('text')
-                .data(edges)
-                .enter()
-                .append('text')
-                .text(function (d) {
-                    return d.relation
-                });
+                .attr('stroke', '#6A6A6A')
+                .attr('stroke-width', .5);
             let gs = g.selectAll('.circleText')
                 .data(nodes)
                 .enter()
@@ -112,107 +58,22 @@
                     let cirX = d.x;
                     let cirY = d.y;
                     return 'translate(' + cirX + ',' + cirY + ')'
-                })
-                .call(d3.drag()
-                    .on('start', started)
-                    .on('drag', dragged)
-                    .on('end', ended)
-                );
+                });
+
             gs.append('circle')
-                .attr('r', 10)
-                .attr('fill', function (d, i) {
-                    return colorScale(i)
-                });
-            gs.append('text')
-                .attr('x', -10)
-                .attr('y', -20)
-                .attr('dy', 10)
-                .text(function (d) {
-                    return d.name
-                });
+                .attr('r', 1.5)
+                .attr('fill', node => {if (node.type === 'o'){ return '#171616'} else { return '#D73D1C'}});
+
+
             function ticked () {
                 links
                     .attr('x1', function (d) { return d.source.x })
                     .attr('y1', function (d) { return d.source.y })
                     .attr('x2', function (d) { return d.target.x })
-                    .attr('y2', function (d) { return d.target.y })
-                linksText
-                    .attr('x', function (d) { return (d.source.x + d.target.x) / 2 })
-                    .attr('y', function (d) { return (d.source.y + d.target.y) / 2 })
+                    .attr('y2', function (d) { return d.target.y });
                 gs
                     .attr('transform', function (d) { return 'translate(' + d.x + ',' + d.y + ')' })
             }
-            function started (d) {
-                if (!d3.event.active) {
-                    forceSimulation.alphaTarget(0.8).restart() // 设置衰减系数，对节点位置移动过程的模拟，数值越高移动越快，数值范围[0, 1]
-                }
-                d.fx = d.x;
-                d.fy = d.y;
-            }
-            function dragged (d) {
-                d.fx = d3.event.x;
-                d.fy = d3.event.y;
-            }
-            function ended (d) {
-                if (!d3.event.active) {
-                    forceSimulation.alphaTarget(0)
-                }
-                d.fx = null;
-                d.fy = null;
-            }
-        },
-        methods: {
-            makeGraph(svgClass) {
-                const links = this.links.map(d => Object.create(d));
-                const nodes = this.nodes.map(d => Object.create(d));
-                console.log('nodes on top');
-                console.log(nodes);
-                console.log('links on top');
-                console.log(links);
-
-                const simulation = d3.forceSimulation(nodes)
-                    .force("link", d3.forceLink(links).id(d => d.id))
-                    .force("charge", d3.forceManyBody())
-                    .force("center", d3.forceCenter(100, 100));
-                console.log('simulation');
-                console.log(simulation);
-
-                const svg = d3.select(svgClass);
-
-                console.log('svg');
-                console.log(svg);
-
-                const link = svg.select('links')
-                                .data(links)
-                                .enter()
-                                .append('line')
-                                .attr('stroke', '#000000')
-                                .attr('stroke-width', 1.5);
-
-                console.log('link');
-                console.log(link);
-                const node = svg.select('nodes')
-                                .data(nodes)
-                                .enter()
-                                .append('circle')
-                                .attr('r', 20)
-                                .attr('fill', '#000000');
-
-                console.log('node');
-                console.log(node);
-
-                simulation.on("tick", () => {
-                    link
-                        .attr("x1", d => d.source.x)
-                        .attr("y1", d => d.source.y)
-                        .attr("x2", d => d.target.x)
-                        .attr("y2", d => d.target.y);
-
-                    node
-                        .attr("cx", d => d.x)
-                        .attr("cy", d => d.y);
-                });
-            },
         },
     }
 
@@ -228,12 +89,10 @@
         width: 95%;
         height: 250px;
         border-radius: 40%;
-        background-color: red;
     }
     .carbon-svg{
         width: 100%;
         height: 100%;
-        background-color: blue;
         border-radius: 40%;
     }
 
