44f5c9496cd3055b6d1afe8d019783e0ca865f48
michaelmarigliano <michael@sharkcell.dk>
2020-11-20 14:22:23 +0100

    changed form text and svg to a card because its easier from now and hoepfully wont be a pain inthe ass later :)

--- captis/src/components/Solution.vue
+++ captis/src/components/Solution.vue
@@ -1,22 +1,50 @@
 <template>
     <v-container class="solution-container">
-        <v-container class = "solution-svg-container">
-            <v-col>
-                <v-row>
-                    <svg class="solution-svg"></svg>
-                </v-row>
-            </v-col>
-        </v-container>
-        <v-container class = "solution-text-container">
-            <v-col>
-                <v-row><h1 class="how">How</h1></v-row>
-                <v-row>
-                    <p class="solution-text">{{messagePart1}}</p>
-                    <p class="solution-text">{{messagePart2}}</p>
-<!--                    <p class="solution-text" v-dummy = "150"></p>-->
-                </v-row>
-            </v-col>
-        </v-container>
+        <v-card
+                class="mx-auto"
+                max-width=100%
+                elevation="0"
+                color="transparent"
+        >
+            <v-list-item>
+                <v-list-item-avatar
+                        tile
+                        size="500"
+                        color="transparent"
+                >
+                    <img src="../assets/marketPics/solution.png">
+                </v-list-item-avatar>
+                <v-list-item-content>
+                    <v-list-item-title class="how">HOW</v-list-item-title>
+                    <v-list-item-title class="title mb-1 text subtitle">we do what we do</v-list-item-title>
+                    <v-list-item-content class="solution-text text">
+                        <p class="solution-text">{{messagePart1}}</p>
+                    </v-list-item-content>
+                    <v-list-item-content class="solution-text text">
+                        <p class="solution-text">{{messagePart2}}</p>
+                    </v-list-item-content>
+                </v-list-item-content>
+
+
+            </v-list-item>
+        </v-card>
+<!--        <v-container class = "solution-svg-container">-->
+<!--            <v-col>-->
+<!--                <v-row>-->
+<!--                    <svg class="solution-svg"></svg>-->
+<!--                </v-row>-->
+<!--            </v-col>-->
+<!--        </v-container>-->
+<!--        <v-container class = "solution-text-container">-->
+<!--            <v-col>-->
+<!--                <v-row><h1 class="how">How</h1></v-row>-->
+<!--                <v-row>-->
+<!--                    <p class="solution-text">{{messagePart1}}</p>-->
+<!--                    <p class="solution-text">{{messagePart2}}</p>-->
+<!--&lt;!&ndash;                    <p class="solution-text" v-dummy = "150"></p>&ndash;&gt;-->
+<!--                </v-row>-->
+<!--            </v-col>-->
+<!--        </v-container>-->
     </v-container>
 </template>
 
@@ -42,28 +70,31 @@
 
 <style scoped>
     .how{
-        width: 100%;
+        display: flex;
+        flex-direction: column;
+        text-align: right;
+        font-family: "Trebuchet MS";
+        font-style: normal;
+        font-weight: bold;
+        font-size: 36px;
+        color: #27214C;
+    }
+    .subtitle{
+        display: flex;
+        flex-direction: column;
+        text-align: right;
+        font-family: "Trebuchet MS";
+        font-style: normal;
+        font-weight: bold;
+        font-size: 36px;
+        color: #61BFF0;
     }
     .solution-container{
         display: flex;
         justify-content: space-between;
         align-content: space-between;
         padding-top: 10px;
-    }
-
-    .solution-text-container{
-        display: flex;
-        flex-direction: column;
-        text-align: right;
-        font-family: "Trebuchet MS";
-        font-style: normal;
-        font-size: 20px;
-        color: #27214C;
-    }
-
-    .solution-svg-container{
-        display: flex;
-        flex-direction: column-reverse;
+        height: 500px;
     }
 
     .solution-text{
@@ -72,15 +103,39 @@
         font-family: "Trebuchet MS", serif;
         font-size: 20px;
         color: #2D2B1F;
+        background-color: white;
     }
 
-    .solution-svg{
-        width: 400px;
-        height: 600px;
-        display: inline;
-        background-color: #61BFF0;
-        /*background-color: white;*/
-        margin-left: 20px;
-        margin-top: 30px;
-    }
+    /*.solution-text-container{*/
+    /*    display: flex;*/
+    /*    flex-direction: column;*/
+    /*    text-align: right;*/
+    /*    font-family: "Trebuchet MS";*/
+    /*    font-style: normal;*/
+    /*    font-size: 20px;*/
+    /*    color: #27214C;*/
+    /*}*/
+
+    /*.solution-svg-container{*/
+    /*    display: flex;*/
+    /*    flex-direction: column-reverse;*/
+    /*}*/
+
+    /*.solution-text{*/
+    /*    display: inline;*/
+    /*    text-align: right;*/
+    /*    font-family: "Trebuchet MS", serif;*/
+    /*    font-size: 20px;*/
+    /*    color: #2D2B1F;*/
+    /*}*/
+
+    /*.solution-svg{*/
+    /*    width: 400px;*/
+    /*    height: 600px;*/
+    /*    display: inline;*/
+    /*    background-color: #61BFF0;*/
+    /*    !*background-color: white;*!*/
+    /*    margin-left: 20px;*/
+    /*    margin-top: 30px;*/
+    /*}*/
 </style>
