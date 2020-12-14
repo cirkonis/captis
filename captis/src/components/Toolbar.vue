aef01efdb3caf7aeafc0df1190ea5b021166c7bc
michaelmarigliano <michael@sharkcell.dk>
2020-12-14 22:01:41 +0100

    fixed tool bar reformatted text and styling

--- captis/src/components/Toolbar.vue
+++ captis/src/components/Toolbar.vue
@@ -1,69 +1,45 @@
 <template>
     <v-toolbar class="theToolbarItself"
-            dense
-            flat
-            color="white"
-        >
-        <v-toolbar-title class="toolbar-title
-        text-xs-body-2
-        text-sm-body-1
-        text-md-h6
-        text-lg-h5
-        text-xl-h4" color="#27214C">
-        <span>{{title}}</span>
+               dense
+               flat
+               color="white"
+    >
+        <v-toolbar-title class="toolbar-title" color="#27214C">
+            <span>{{title}}</span>
         </v-toolbar-title>
-        <v-spacer> </v-spacer>
+        <v-spacer></v-spacer>
         <v-toolbar-items class="toolbar-buttons">
-            <v-btn class = "toolbar-button
-        text-xs-body-2
-        text-sm-body-1
-        text-md-h6
-        text-lg-h6
-        text-xl-h5"
+            <v-btn class="toolbar-button"
                    text color="#27214C"
+                   style="font-size: 1.5vw"
                    v-on:click.native="buttonWorks(buttonNames[0].buttonName)"
                    v-scroll-to="'#main'">
                 {{buttonNames[0].buttonName}}
             </v-btn>
-            <v-btn class = "toolbar-button
-        text-xs-body-2
-        text-sm-body-1
-        text-md-h6
-        text-lg-h6
-        text-xl-h5"
+            <v-btn class="toolbar-button"
                    text color="#27214C"
+                   style="font-size: 1.5vw"
                    v-on:click.native="buttonWorks(buttonNames[1].buttonName)"
                    v-scroll-to="'#problem'">
                 {{buttonNames[1].buttonName}}
             </v-btn>
-            <v-btn class = "toolbar-button
-        text-xs-body-2
-        text-sm-body-1
-        text-md-h6
-        text-lg-h6
-        text-xl-h5"
+            <v-btn class="toolbar-button"
                    text color="#27214C"
+                   style="font-size: 1.5vw"
                    v-on:click.native="buttonWorks(buttonNames[2].buttonName)"
-                   v-scroll-to="'#solution'" >
+                   v-scroll-to="'#solution'">
                 {{buttonNames[2].buttonName}}
             </v-btn>
-            <v-btn class = "toolbar-button
-        text-xs-body-2
-        text-sm-body-1
-        text-md-h6
-        text-lg-h6
-        text-xl-h5"
+            <v-btn class="toolbar-button"
                    text color="#27214C"
+                   style="font-size: 1.5vw"
                    v-on:click.native="buttonWorks(buttonNames[3].buttonName)"
                    v-scroll-to="'#bizniz'">
                 {{buttonNames[3].buttonName}}
             </v-btn>
-            <v-btn class = "toolbar-button
-        text-xs-body-2
-        text-sm-body-1
-        text-md-h6
-        text-lg-h6
-        text-xl-h5" text color="#27214C"
+            <v-btn class="toolbar-button"
+                   text color="#27214C"
+                   style="font-size: 1.5vw"
                    v-on:click.native="buttonWorks(buttonNames[4].buttonName)"
                    v-scroll-to="'#who'">
                 {{buttonNames[4].buttonName}}
@@ -75,20 +51,20 @@
 <script>
     export default {
         name: "Toolbar",
-            data: function () {
-                return {
-                    title: "CAPTIS",
-                    buttonNames: [
-                        {buttonName: "Main"},
-                        {buttonName: "What"},
-                        {buttonName: "How"},
-                        {buttonName: "Why"},
-                        {buttonName: "Who"},
-                        ]
-                }
-            },
+        data: function () {
+            return {
+                title: "CAPTIS",
+                buttonNames: [
+                    {buttonName: "Main"},
+                    {buttonName: "What"},
+                    {buttonName: "How"},
+                    {buttonName: "Why"},
+                    {buttonName: "Who"},
+                ]
+            }
+        },
         methods: {
-            buttonWorks(){
+            buttonWorks() {
             },
             scrollMeTo(refName) {
                 const element = this.$els[refName];
@@ -101,20 +77,14 @@
 </script>
 
 <style scoped>
-    .toolbar-title{
-        font-family: "Trebuchet MS", serif;
-        font-size: 24px;
+    .toolbar-title {
+        font-family: titillium;
+        font-size: 2.5vw;
         color: #27214C;
         opacity: .5;
     }
 
-    .toolbar-button{
-        font-family: "Trebuchet MS", serif;
-        font-size: 16px;
-    }
-
-    .captisTech{
-        height:1px;
-        width:1px;
+    .toolbar-button {
+        font-family: titillium;
     }
 </style>
